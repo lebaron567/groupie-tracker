2431b6fc9338142dcbeea67a00c74254fba9a5d9
Emeric barotin <emericbarotin@etu.univ-nantes.fr>
2023-02-09 16:16:30 +0100

    :construction:  | recupair api

--- linkApi.go
+++ linkApi.go
@@ -19,19 +19,80 @@ type groupe struct {
 	Relations    string   `json:"relations"`
 }
 
-var g []groupe
+type location struct {
+	Id        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
+}
+
+type date struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
 
 // {"artists":"https://groupietrackers.herokuapp.com/api/artists","locations":"https://groupietrackers.herokuapp.com/api/locations","dates":"https://groupietrackers.herokuapp.com/api/dates","relation":"https://groupietrackers.herokuapp.com/api/relation"}
-func RecupInfo() {
+func RecupArtists() []groupe {
+	var g []groupe
 	url := "https://groupietrackers.herokuapp.com/api/artists" // adresse url
 	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
+	res, erre := http.DefaultClient.Do(req)
+	if erre != nil {
+		fmt.Println("Error", erre)
+	}
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(body))
+	//fmt.Println(string(body))
 	err := json.Unmarshal([]byte(body), &g)
 	if err != nil {
 		fmt.Println("Error", err)
+	} else {
+		fmt.Println("Person:", g[1])
 	}
-	fmt.Println("Person:", g[0])
+	return g
+}
+
+func RecupDates(g []groupe) []date {
+	var listDate []date
+	for i := 0; i < len(g); i++ {
+		url := g[i].ConcertDates // adresse url
+		req, _ := http.NewRequest("GET", url, nil)
+		res, erre := http.DefaultClient.Do(req)
+		if erre != nil {
+			fmt.Println("Error", erre)
+		}
+		var d date
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		//fmt.Println(string(body))
+		err := json.Unmarshal([]byte(body), &d)
+		if err != nil {
+			fmt.Println("Error", err)
+		} else {
+			listDate = append(listDate, d)
+		}
+	}
+	fmt.Print(listDate)
+	return listDate
+}
+
+func RecupLocation(g []groupe) []location {
+	var listDate []location
+	for i := 0; i < len(g); i++ {
+		url := g[i].Locations // adresse url
+		req, _ := http.NewRequest("GET", url, nil)
+		res, erre := http.DefaultClient.Do(req)
+		if erre != nil {
+			fmt.Println("Error", erre)
+		}
+		var l location
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		err := json.Unmarshal([]byte(body), &l)
+		if err != nil {
+			fmt.Println("Error", err)
+		} else {
+			listDate = append(listDate, l)
+		}
+	}
+	return listDate
 }
