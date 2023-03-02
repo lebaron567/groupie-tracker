f1d7dedb6a66569b2f8e75a547f7d00580ccc032
lucas.bigot@ynov.com <lucas.bigot@ynov.com>
2023-03-02 13:39:50 +0100

    :sparkles: | introduce new sort systeme

--- server.go
+++ server.go
@@ -1,9 +1,11 @@
 package groupieTrackers
 
-import "text/template"
+import (
+	"text/template"
+)
 
 // Function that load all templates for all pages
-func LoadTemplates() (*template.Template, *template.Template, *template.Template,*template.Template) {
+func LoadTemplates() (*template.Template, *template.Template, *template.Template, *template.Template) {
 	homePage := template.Must(template.ParseFiles("./front/index.html"))
 	artistPage := template.Must(template.ParseFiles("./front/artiste.html"))
 	locationPage := template.Must(template.ParseFiles("./front/location.html"))
@@ -13,16 +15,23 @@ func LoadTemplates() (*template.Template, *template.Template, *template.Template
 }
 
 func SearchGroupe(nameSearch string, g []groupe) []groupe {
+	g2 := []groupe{}
 	for index, element := range g {
 		if element.Name == nameSearch {
-			save := g[index]
-			g[index] = g[0]
-			g[index].IsSearch = false
-			g[0] = save
-			g[0].IsSearch = true
+			g2 = append(g2, element)
+			g2[0].IsSearch = true
 		} else {
 			g[index].IsSearch = false
 		}
 	}
-	return g
+	for _, element := range g {
+		if element.Name != nameSearch {
+			g2 = append(g2, element)
+		}
+	}
+	return g2
+}
+
+func trieAlphabetik(g []groupe){
+	
 }
