9dd1bd987b7130f64862783e355260e19ef10631
lucas.bigot@ynov.com <lucas.bigot@ynov.com>
2023-03-13 13:09:30 +0100

    :rocket: | end of pagination

--- main/main.go
+++ main/main.go
@@ -15,10 +15,8 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 	//récuperer les info de l'"api"
 	listGroups := groupieTrackers.RecupInfo()
-	newlistGroups := groupieTrackers.DiviserEnListeDeXelement(listGroups, 52)
+	newlistGroups := groupieTrackers.DiviserEnListeDeXelement(listGroups, len(listGroups))
 	numberPageChoice := 0
-
-	// listGroupsPage1, listGroupsPage2 := groupieTrackers.DiviserEnDeux(listGroups)
 
 	// Load the first page of the game
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +30,9 @@ func main() {
 	http.HandleFunc("/artiste", func(w http.ResponseWriter, r *http.Request) {
 		numberOfItemsOnPage := r.FormValue("numberPage")
 		pageChoice := r.FormValue("page")
+		searchUser := r.FormValue("userSearch")
+		sortingChoices := r.FormValue("sortingChoices")
+
 		if pageChoice != "" {
 			if pageChoice == "precedent" && numberPageChoice > 0 {
 				numberPageChoice--
@@ -39,44 +40,24 @@ func main() {
 				numberPageChoice++
 			}
 		} else {
-			if numberOfItemsOnPage != "" {
-				listGroups = groupieTrackers.RecupInfo()
-				number, _ := strconv.Atoi(numberOfItemsOnPage)
-				newlistGroups = groupieTrackers.DiviserEnListeDeXelement(listGroups, number)
+			numberPageChoice = 0
+			if searchUser != "" {
+				newlistGroups = groupieTrackers.SearchGroupe(searchUser, newlistGroups)
+			} else if sortingChoices != "" {
+				newlistGroups = groupieTrackers.SortElement(sortingChoices, len(newlistGroups[0]))
 			} else {
-				listGroups = groupieTrackers.RecupInfo()
-				newlistGroups = groupieTrackers.DiviserEnListeDeXelement(listGroups, 52)
+				if numberOfItemsOnPage != "" {
+					number, _ := strconv.Atoi(numberOfItemsOnPage)
+					newlistGroups = groupieTrackers.ReconstituerEtDiviserEnListeDeXelement(newlistGroups, number)
+				} else {
+					newlistGroups = groupieTrackers.ReconstituerEtDiviserEnListeDeXelement(newlistGroups, 52)
+				}
 			}
 		}
-		/*
-			if sortingChoices != "" {
-				listGroups = groupieTrackers.RecupInfo()
-				if sortingChoices == "AscendingAlphabeticalSorting" {
-					listGroups = groupieTrackers.AscendingAlphabeticalSorting(listGroups)
-				} else if sortingChoices == "DescendingAlphabeticalSorting" {
-					listGroups = groupieTrackers.DescendingAlphabeticalSorting(listGroups)
-				} else if sortingChoices == "SortingAscendingCreationDate" {
-					listGroups = groupieTrackers.SortingCreationDate(listGroups, true)
-				} else if sortingChoices == "SortingDescendingCreationDate" {
-					listGroups = groupieTrackers.SortingCreationDate(listGroups, false)
-				} else if sortingChoices == "BubbleSortByNumberMemberAscending" {
-					listGroups = groupieTrackers.BubbleSortByNumberMemberAscending(listGroups)
-				} else if sortingChoices == "BubbleSortByNumberMemberDescending" {
-					listGroups = groupieTrackers.BubbleSortByNumberMemberDescending(listGroups)
-				}
-			}
-				sortingChoices := r.FormValue("sortingChoices")
-
-				if searchUser != "" {
-					listGroups = groupieTrackers.SearchGroupe(searchUser, listGroups)
-				} else {
-					listGroups[0].IsSearch = true
-				}
-				searchUser := r.FormValue("userSearch")
-		*/
-
 		listGroups = newlistGroups[numberPageChoice]
-		listGroups[0].NumberOfPage = len(newlistGroups)
+		if searchUser == "" {
+			listGroups[0].IsSearch = true
+		}
 		artistPage.Execute(w, listGroups)
 	})
 
@@ -89,4 +70,8 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func RecupInfo() {
+	panic("unimplemented")
 }
--- server.go
+++ server.go
@@ -14,22 +14,43 @@ func LoadTemplates() (*template.Template, *template.Template, *template.Template
 	return homePage, artistPage, locationPage, concertPage
 }
 
-func SearchGroupe(nameSearch string, g []groupe) []groupe {
+func SortElement(sortingChoices string, lenNewlistGroups int) [][]groupe {
+	listGroups := RecupInfo()
+	if sortingChoices == "AscendingAlphabeticalSorting" {
+		listGroups = AscendingAlphabeticalSorting(listGroups)
+	} else if sortingChoices == "DescendingAlphabeticalSorting" {
+		listGroups = DescendingAlphabeticalSorting(listGroups)
+	} else if sortingChoices == "SortingAscendingCreationDate" {
+		listGroups = SortingCreationDate(listGroups, true)
+	} else if sortingChoices == "SortingDescendingCreationDate" {
+		listGroups = SortingCreationDate(listGroups, false)
+	} else if sortingChoices == "BubbleSortByNumberMemberAscending" {
+		listGroups = BubbleSortByNumberMemberAscending(listGroups)
+	} else if sortingChoices == "BubbleSortByNumberMemberDescending" {
+		listGroups = BubbleSortByNumberMemberDescending(listGroups)
+	}
+	nlistGroups := DiviserEnListeDeXelement(listGroups, lenNewlistGroups)
+	return nlistGroups
+}
+
+func SearchGroupe(nameSearch string, artistGroup [][]groupe) [][]groupe {
+	newArtistGroup := ReconstituerList(artistGroup)
 	g2 := []groupe{}
-	for index, element := range g {
+	for index, element := range newArtistGroup {
 		if element.Name == nameSearch {
 			g2 = append(g2, element)
 			g2[0].IsSearch = true
 		} else {
-			g[index].IsSearch = false
+			newArtistGroup[index].IsSearch = false
 		}
 	}
-	for _, element := range g {
+	for _, element := range newArtistGroup {
 		if element.Name != nameSearch {
 			g2 = append(g2, element)
 		}
 	}
-	return g2
+	newArtistGroup2 := DiviserEnListeDeXelement(g2, len(artistGroup[0]))
+	return newArtistGroup2
 }
 
 func DiviserEnListeDeXelement(artistGroup []groupe, x int) [][]groupe {
@@ -42,5 +63,21 @@ func DiviserEnListeDeXelement(artistGroup []groupe, x int) [][]groupe {
 			page = []groupe{}
 		}
 	}
+	return newArtistGroup
+}
+
+func ReconstituerList(artistGroup [][]groupe) []groupe {
+	newGroupe := []groupe{}
+	for index := range artistGroup {
+		for index2 := range artistGroup[index] {
+			newGroupe = append(newGroupe, artistGroup[index][index2])
+		}
+	}
+	return newGroupe
+}
+
+func ReconstituerEtDiviserEnListeDeXelement(artistGroup [][]groupe, x int) [][]groupe {
+	newGroupe := ReconstituerList(artistGroup)
+	newArtistGroup := DiviserEnListeDeXelement(newGroupe, x)
 	return newArtistGroup
 }
